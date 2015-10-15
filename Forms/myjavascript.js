c3aca2e97fd411d242fbb817499d39cb05445c46
lizbachmann <lbachmann06@gmail.com>
2015-10-14 19:06:44 -0500

    javascript

---
+++ Forms/myjavascript.js
@@ -0,0 +1,40 @@
+//alert("hello world");
+var name = "Liz";
+console.log(name);
+console.log("hello");
+
+//Numbers
+var x= "3";
+var y= "8";
+console.log(x+y);
+
+console.log(x*y);
+console.log(x-2);
+console.log(y+3);
+//Strings
+var firstName= "Liz";
+var lastName= "Bachmann";
+console.log(firstName + " " + lastName);
+console.log(firstName + 3);
+//boolean
+var one = Boolean(4);
+var two= Boolean(11);
+console.log(one >two);
+console.log(one.length < two.length);
+//Arrays
+var town=["Lincoln", "London", "Sydney"];
+var state=["Nebraska","Florida","California"]
+console.log(town[2]);
+console.log(town.length);
+console.log(town+ " " + state);
+
+var size=["XS","S","M","L"];
+
+//objects
+var human ={
+    name: "Liz",
+    age: 24,
+    location: "Lincoln",
+};
+
+console.log (human["name"] + " is " + human["age"]+ " and lives in " + human["location"]);
